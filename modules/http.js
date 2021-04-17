a3fd89df6ecd36fbd0557e970a7e642bcbc2494a
Mark Burggraf <markb@mantisbible.com>
2021-04-17 16:31:47 -0700

    fixes

--- modules/http.js
+++ modules/http.js
@@ -25,108 +25,40 @@ module.exports.header = function(url, value, headers = null) {
     sql(`select * from http(('HEADER','${url}','${format_headers(headers)}', null, '${encodeURIComponent(value)}'))`)[0];
     return retval;    
 }
-/*
-module.exports.header = function(url, value) {
-    const retval = sql(`select * from http_header('${url}', ${value})`)[0];
-    return retval;
-};
-*/
 module.exports.head = function(url, headers = null) {
     const retval = 
     sql(`select * from http(('HEAD','${url}','${format_headers(headers)}', null, null))`)[0];
     return retval;    
 }
-/*
-module.exports.head = function(url) {
-    const retval = sql(`select * from http_head('${url}')`)[0];
-    return retval;
-};
-*/
-/*
-
-    ARRAY[http_header('Authorization','Basic ' || encode(MAILGUN_API_KEY::bytea,'base64'::text))],
-
-*/
 module.exports.get = function(url, headers = null) {
-    // const retval = 
-    // sql(`select * from http(('GET','${url}','{"(Authorization,\\"Basic YXBpOmtleS0xMDY3MDZkODU2YjVkN2JmZDYwNjAzZGVlOWE2YjljNg==\\")"}', null, null))`)[0];
     const retval = 
     sql(`select * from http(('GET','${url}','${format_headers(headers)}', null, null))`)[0];
     
     return retval;    
 }
-/*
-module.exports.get = function(url) {
-    const retval = sql(`select * from http_get('${url}')`)[0];
-    return retval;
-};
-*/
 module.exports.delete = function(url, headers = null) {
     const retval = 
     sql(`select * from http(('DELETE','${url}','${format_headers(headers)}', null, null))`)[0];
     return retval;    
 }
-/*
-module.exports.delete = function(url) {
-    const retval = sql(`select * from http_delete('${url}')`)[0];
-    return retval;
-};
-*/
 module.exports.post = function(url, payload, headers = null, content_type) {
     const params = serializeObject(payload);;
     const retval = 
     sql(`select * from http(('POST','${url}','${format_headers(headers)}', '${content_type || 'application/x-www-form-urlencoded'}', '${params}'))`)[0];
     return retval;    
 }
-/*
-module.exports.post = function(url, payload, content_type) {
-    const params = serializeObject(payload);;
-    const retval = sql(`select * from http_post('${url}','${params}',${content_type || 'application/x-www-form-urlencoded'})`)[0];
-    return retval;
-}
-*/
 module.exports.put = function(url, payload, headers = null, content_type) {
     const params = serializeObject(payload);;
     const retval = 
     sql(`select * from http(('PUT','${url}','${format_headers(headers)}','${content_type || 'application/x-www-form-urlencoded'}', '${params}'))`)[0];
     return retval;    
 }
-/*
-module.exports.put = function(url, payload, content_type) {
-    const params = serializeObject(payload);;
-    const retval = sql(`select * from http_put('${url}','${params}',${content_type || 'application/x-www-form-urlencoded'})`)[0];
-    return retval;
-}
-*/
 module.exports.patch = function(url, payload, headers = null, content_type) {
     const params = serializeObject(payload);;
     const retval = 
     sql(`select * from http(('PATCH','${url}','${format_headers(headers)}','${content_type || 'application/x-www-form-urlencoded'}', '${params}'))`)[0];
     return retval;    
 }
-/*
-module.exports.patch = function(url, payload, content_type) {
-    const params = serializeObject(payload);;
-    const retval = sql(`select * from http_patch('${url}','${params}',${content_type || 'application/x-www-form-urlencoded'})`)[0];
-    return retval;
-}
-*/
-
-
-/*
-const options = {
-    hostname: 'whatever.com',
-    port: 443,
-    path: '/todos',
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Content-Length': data.length
-    }
-  }
-  */
-
-// {'mykey':'myvalue','mynumber':23,'myjson':{'firstname':'harry','lastname':'styles'}}
 
 const serializeObject = function(obj) {
     var pairs = [];
@@ -146,7 +78,6 @@ const serializeObject = function(obj) {
 const format_headers = function(headers) {
     let headers_block;
     if (headers !== null) {
-        // '{"(Authorization,\\"Basic YXBpOmtleS0xMDY3MDZkODU2YjVkN2JmZDYwNjAzZGVlOWE2YjljNg==\\")"}'
         headers_block = '{';
         headers.map((header) => {
             headers_block += 
